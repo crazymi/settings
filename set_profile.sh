aafacd8de4982763cf28ca97dc2e2a0dcb70cc86
TAZO <crazymi@snu.ac.kr>
2018-08-22 14:39:59 +0900

    Remove unnecessary append
    The removed line cause func_profile to run every time when user login

--- set_profile.sh
+++ set_profile.sh
@@ -4,9 +4,6 @@
 if [ ! -f ~/func_profile ]; then
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     cp ${DIR}/scripts/func_profile ~/
-    echo "if [ -f ~/func_profile ]; then
-        . ~/func_profile
-    fi" >> ~/.bashrc
 fi
 
 ## profile
